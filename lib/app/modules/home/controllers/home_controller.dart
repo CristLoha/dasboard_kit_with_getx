db73802a5f1da5d34a6b2e0ef1337b168f77bbee
cristloha <cristloha2205@gmail.com>
2022-09-05 16:42:33 +0800

    Update Controller

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -2,12 +2,12 @@ import 'package:get/get.dart';
 
 class HomeController extends GetxController {
   RxInt tabIndex = 0.obs;
-  RxBool isExpanded = false.obs;
 
-  RxList<bool> selected = [true, false, false, false, false].obs;
+  RxList<bool> selected =
+      [true, false, false, false, false, false, false, false, false].obs;
 
   void select(int n) {
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 8; i++) {
       if (i != n) {
         selected[i] = false;
       } else {
